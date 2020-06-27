6fc2878572ba2884425b123f908486cd85a33a1a
Gui-dev <adrianon10@gmail.com>
2020-06-27 15:03:17 -0300

    first commit

---
+++ backend/src/App.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+
+import routes from './routes'
+
+class App {
+
+  public app: express.Application
+
+  constructor() {
+    this.app = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  private middlewares() {
+    this.app.use( express.json() )
+    this.app.use( cors() )
+  }
+
+  private routes() {
+    this.app.use( routes )
+  }
+}
+
+export default new App().app
