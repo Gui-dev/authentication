a5e62db9635007343b0c17bb7716390c6678fd08
Gui-dev <adrianon10@gmail.com>
2020-07-13 21:09:40 -0300

    Fix: validação

--- mobile/src/validations/authValidations.ts
+++ mobile/src/validations/authValidations.ts
@@ -8,7 +8,7 @@ export const signInValidation = Yup.object().shape( {
 export const signUpValidation = Yup.object().shape( {
   name: Yup.string().required( 'Campo nome é obrigátorio' ),
   email: Yup.string().email().required( 'Campo email é obrigátorio' ),
-  password: Yup.string().min(6, 'Tem que ter mais de 6 caracteres').required( 'Campo senha é obrigátorio' ),
+  password: Yup.string().min(6, 'Senha tem que ter mais de 6 caracteres').required( 'Campo senha é obrigátorio' ),
   confirmPassword: Yup.string()
     .oneOf( [ Yup.ref( 'password' ) ], 'Senhas não conferem' )
     .required( 'Campo confirmar senha é obrigátorio' )
