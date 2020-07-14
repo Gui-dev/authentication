073f4c576ac027f1b33d941afce18309400fbe05
Gui-dev <adrianon10@gmail.com>
2020-07-14 03:33:14 -0300

    Finalizando a page Home

--- mobile/src/screens/App/Home/index.tsx
+++ mobile/src/screens/App/Home/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
 
-import { Container } from './style'
+import api from './../../../services/api'
+import { useAuth } from './../../../contexts/auth'
+
+import { Container, Title, Person, ButtonLink, ButtonLinkText } from './style'
+
+interface UserResponse {
+  id: string,
+  name: string,
+  email: string,
+}
 
 const Home: React.FC = () => {
   
+  const { signOut } = useAuth()
+  const [ user, setUser ] = useState<UserResponse | null>( null ) 
+
+  useEffect( () => {
+    async function loadUser() {
+      const { data } = await api.get<UserResponse>( '/users' )
+      setUser( data )
+    }
+
+    loadUser()
+  }, [] )
+
+  const handleSignOut = () => {
+    signOut()
+  }
+
   return (
     <Container>
-      <Text>Welcome</Text>
+      <Title>
+        Welcome, <Person>{ user?.name }</Person>
+      </Title>
+      <ButtonLink onPress={ handleSignOut }>
+        <ButtonLinkText>
+          Sair do app
+        </ButtonLinkText>
+      </ButtonLink>
     </Container>
   )
 }
--- mobile/src/screens/App/Home/style.ts
+++ mobile/src/screens/App/Home/style.ts
@@ -1,7 +1,28 @@
 import styled from 'styled-components/native'
+import { TouchableOpacity } from 'react-native'
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`
+
+export const Title = styled.Text`
+  font-size: 16px;
+  color: #333;
+`
+
+export const Person = styled.Text`
+  font-size: 14px;
+  color: #666;
+`
+
+export const ButtonLink = styled(TouchableOpacity)`
+  margin-top: 15px;
+`
+
+export const ButtonLinkText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #09F;
 `
