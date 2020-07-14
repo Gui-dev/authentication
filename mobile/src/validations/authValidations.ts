7602b84e06ae3aaecde6f11c4543cc0cb28aeba3
Gui-dev <adrianon10@gmail.com>
2020-07-13 21:07:15 -0300

    Implementando as validações de signIn e signUp

---
+++ mobile/src/validations/authValidations.ts
@@ -0,0 +1,15 @@
+import * as Yup from 'yup'
+
+export const signInValidation = Yup.object().shape( {
+  email: Yup.string().email().required( 'Campo email é obrigátorio' ),
+  password: Yup.string().required( 'Campo senha é obrigátorio' ),
+} )
+
+export const signUpValidation = Yup.object().shape( {
+  name: Yup.string().required( 'Campo nome é obrigátorio' ),
+  email: Yup.string().email().required( 'Campo email é obrigátorio' ),
+  password: Yup.string().min(6, 'Tem que ter mais de 6 caracteres').required( 'Campo senha é obrigátorio' ),
+  confirmPassword: Yup.string()
+    .oneOf( [ Yup.ref( 'password' ) ], 'Senhas não conferem' )
+    .required( 'Campo confirmar senha é obrigátorio' )
+} )
